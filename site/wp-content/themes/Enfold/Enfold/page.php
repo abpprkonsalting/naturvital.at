c8748a490f4407a199554283e2e6ea41ea711637
Luis Miguel <lmpf1982@gmail.com>
2019-08-06 13:43:31 -0400

    home primera version

--- site/wp-content/themes/Enfold/Enfold/page.php
+++ site/wp-content/themes/Enfold/Enfold/page.php
@@ -13,9 +13,10 @@
  	 
  	 do_action( 'ava_after_main_title' );
 	 ?>
-
+	 
+}
 		<div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
-
+		
 			<div class='container'>
 
 				<main class='template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>
