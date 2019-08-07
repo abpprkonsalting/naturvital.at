b95ad439747b126adf2b87b15e7cf2990dde1cc4
Armando Baños Pascual <armbp1972@gmail.com>
2019-08-07 09:59:54 +0200

    sección de botones funcionando en frontend

--- site/wp-content/plugins/abp_init_script.js
+++ site/wp-content/plugins/abp_init_script.js
@@ -7,6 +7,23 @@
         if ( jQuery('body').hasClass("home")) {
 
 
+            var buttons = jQuery("#av-layout-grid-1 .flex_cell_inner .avia-button");
+            buttons.each( function(){
+
+                jQuery(this).attr('href','');
+                jQuery(this).click(buttons, function($event){
+
+                    $event.preventDefault();
+                    $event.data.css('background','transparent');
+                    $event.data.css('color','#017635');
+
+                    jQuery(this).css('background','#017635');
+                    jQuery(this).css('color','white');
+                });
+            });
+
+
+
         }
     
     });
